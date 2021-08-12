6fd463b4b3b10cd0595ebfbcc8ae85ab6fa14755
Jun-T-git <jun0303t@icloud.com>
2021-08-12 13:51:07 +0900

    ドロップダウンメニュー実装

---
+++ src/components/dropDown.tsx
@@ -0,0 +1,49 @@
+import React, { MouseEventHandler } from "react";
+import { Menu } from "@headlessui/react";
+
+type MenuItem = {
+  text: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+type Props = {
+  children: JSX.Element;
+  menuItems: Array<MenuItem>;
+};
+
+/**
+ * ドロップダウンメニュー
+ * @param children メニューを開くボタン
+ * @param menuItems メニューの項目
+ */
+
+const Dropdown: React.VFC<Props> = ({ children, menuItems }) => {
+  return (
+    <Menu>
+      <div className="relative">
+        <Menu.Button>{children}</Menu.Button>
+        <Menu.Items
+          as="div"
+          className="absolute top-[3.75rem] right-0 flex flex-col w-52 border rounded"
+        >
+          {menuItems.map((menuItem, mi) => (
+            <Menu.Item key={mi}>
+              {({ active }) => (
+                <button
+                  className={`${
+                    active ? "bg-gray-300 px-3 py-1" : "bg-white px-3 py-1"
+                  }`}
+                  onClick={menuItem.onClick}
+                >
+                  {menuItem.text}
+                </button>
+              )}
+            </Menu.Item>
+          ))}
+        </Menu.Items>
+      </div>
+    </Menu>
+  );
+};
+
+export default Dropdown;
