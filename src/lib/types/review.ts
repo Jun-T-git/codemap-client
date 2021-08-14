0ebabdb93e5fe20b30402d104bcc3aa029f43fe1
Jun-T-git <jun0303t@icloud.com>
2021-08-14 14:46:50 +0900

    型定義

---
+++ src/lib/types/review.ts
@@ -0,0 +1,10 @@
+export type Review = {
+  id: string;
+  title: string;
+  content: string;
+  difficulty_level: string;
+  user_id: string;
+  book_id: string;
+  created_at: string;
+  updated_at: string;
+};
