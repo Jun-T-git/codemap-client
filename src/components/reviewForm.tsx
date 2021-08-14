f354268b401a9a50a663d8111ec629582120e66b
Jun-T-git <jun0303t@icloud.com>
2021-08-14 18:56:24 +0900

    review投稿フォーム修正

--- src/components/reviewForm.tsx
+++ src/components/reviewForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ReactStars from "react-rating-stars-component";
 import { PostReviewParams, PostReviewRequest } from "~/lib/api/reviews";
 import Button from "./button";
 import Input from "./input";
@@ -24,7 +25,7 @@ const ReviewForm: React.VFC<Props> = ({
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [recommendationLevel, setRecommendationLevel] = useState<number>(5);
-  const [difficultyLevel, setdifficultyLevel] = useState<number>(5);
+  const [difficultyLevel, setDifficultyLevel] = useState<number>(5);
 
   const postReview = async () => {
     const postReviewParams: PostReviewParams = {
@@ -44,8 +45,8 @@ const ReviewForm: React.VFC<Props> = ({
   };
 
   return (
-    <div className="bg-white border rounded px-16 py-10 mx-auto my-7 max-w-3xl flex flex-col space-y-14">
-      <h1 className="text-3xl font-bold text-center">レビューを投稿する</h1>
+    <div className="bg-white border rounded px-5 py-10 mx-auto w-full flex flex-col space-y-14">
+      <h1 className="text-2xl font-bold text-center">レビューを投稿する</h1>
       <div className="flex flex-col space-y-7">
         <Input
           label="タイトル"
@@ -53,23 +54,40 @@ const ReviewForm: React.VFC<Props> = ({
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+        <div className="flex justify-evenly items-center">
+          <div className="flex space-x-2 items-center">
+            <label className="text-gray-700 text-sm md:text-lg font-bold">
+              おすすめ度：
+            </label>
+            <ReactStars
+              count={5}
+              size={24}
+              value={recommendationLevel}
+              isHalf={false}
+              edit={true}
+              onChange={(e) => setRecommendationLevel(Number(e.target.value))}
+            />
+          </div>
+          <div className="flex space-x-2 items-center">
+            <label className="text-gray-700 text-sm md:text-lg font-bold">
+              難易度：
+            </label>
+            <ReactStars
+              count={5}
+              size={24}
+              value={difficultyLevel}
+              isHalf={false}
+              edit={true}
+              onChange={(e) => setDifficultyLevel(Number(e.target.value))}
+            />
+          </div>
+        </div>
         <Input
           label="詳細"
           placeholder="test1@example.com"
           value={content}
+          type={"textarea"}
           onChange={(e) => setContent(e.target.value)}
-        />
-        <Input
-          label="おすすめ度"
-          placeholder="5"
-          value={String(recommendationLevel)}
-          onChange={(e) => setRecommendationLevel(Number(e.target.value))}
-        />
-        <Input
-          label="難易度"
-          placeholder="5"
-          value={String(difficultyLevel)}
-          onChange={(e) => setdifficultyLevel(Number(e.target.value))}
         />
         <Button
           onClick={async () => {
